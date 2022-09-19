b771e5bec4d4865ce30353ec82c4c9521cfac07e
juliacatanz <113040686+juliacatanz@users.noreply.github.com>
2022-09-19 11:10:07 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,22 @@
+input.onButtonPressed(Button.A, function () {
+    player.change(LedSpriteProperty.X, 1)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    player.change(LedSpriteProperty.Y, -1)
+})
+input.onButtonPressed(Button.B, function () {
+    player.change(LedSpriteProperty.Y, -1)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    player.change(LedSpriteProperty.X, 1)
+})
+let player: game.LedSprite = null
+basic.showIcon(IconNames.Heart)
+let enemy = game.createSprite(randint(0, 5), randint(0, 5))
+player = game.createSprite(0, 0)
+basic.forever(function () {
+    if (player.isTouching(enemy)) {
+        basic.showIcon(IconNames.Heart)
+        game.addScore(1)
+    }
+})
